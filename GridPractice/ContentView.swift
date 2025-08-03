5a46e5f09650d2ef38265730b890b0647e540c33
Maimai10808 <331686549@qq.com>
2025-08-03 08:16:26 +0800

    2.  Basic Grid

--- GridPractice/ContentView.swift
+++ GridPractice/ContentView.swift
@@ -10,12 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Grid(alignment: .trailing ,horizontalSpacing: 2, verticalSpacing: 5)
+            {
+                GridRow{
+                    Color.red
+                        .frame(width: 50 ,height: 50)
+                    Color.yellow
+                        .frame(height: 50)
+                }
+                
+                GridRow{
+                    Color.red
+                    Color.yellow
+                    Color.green
+                }
+                
+                GridRow(alignment: .firstTextBaseline){
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                }
+            }
+            
+            Divider()
         }
-        .padding()
+        .padding(.horizontal)
     }
 }
 
